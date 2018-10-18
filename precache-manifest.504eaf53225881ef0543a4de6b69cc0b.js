b707430d2f7b454ef210cec5063efc88d661c9de
Amit Kumar Garg <amitkumar.garg@ck12.org>
2018-10-19 01:49:09 +0530

    Updates

---
+++ precache-manifest.504eaf53225881ef0543a4de6b69cc0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93e54ee079186b436108",
+    "url": "/writing-assist/static/js/runtime~main.93e54ee0.js"
+  },
+  {
+    "revision": "fa5597eab0df0a9ea943",
+    "url": "/writing-assist/static/js/main.fa5597ea.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/writing-assist/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "fa5597eab0df0a9ea943",
+    "url": "/writing-assist/static/css/main.aac0f9a7.chunk.css"
+  },
+  {
+    "revision": "9b07feda06ff4087c213a5a05ebe0b2c",
+    "url": "/writing-assist/index.html"
+  }
+];
