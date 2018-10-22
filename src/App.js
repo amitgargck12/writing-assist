d679399dda74ee5caf2ea39bfa5b599bb239387e
Amit Kumar Garg <amitkumar.garg@ck12.org>
2018-10-22 14:13:20 +0530

    Code refactored and working fine with Red base color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import CanvasComponent from "./components/CanvasComponent";
+import Controls from './components/Controls'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CanvasComponent height="600" width="600"/>
+        <CanvasComponent letter="abcd efgh ijkl mnop qrst" height={(window.innerHeight-60)} width={window.innerWidth}/>
       </div>
     );
   }
--- src/components/CanvasComponent.jsx
+++ src/components/CanvasComponent.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from "react";
 
 export default class CanvasComponent extends Component {
-  constructor() {
-    super();
-    this.state = { lastX: 0, lastY: 0 };
+  constructor(props) {
+    super(props);
+    this.state = { lastX: 0, lastY: 0, letter: props.letter };
     this.handleTouchStart = this.handleTouchStart.bind(this);
     this.handleTouchMove = this.handleTouchMove.bind(this);
     this.handleTouchEnd = this.handleTouchEnd.bind(this);
+    this.handleCompare = this.handleCompare.bind(this);
   }
   componentDidMount() {
-    this.can = document.getElementById("can");
-    this.ctx = this.can.getContext("2d");
+    let can = document.getElementById("can");
+    this.ctx = can.getContext("2d");
+    can.addEventListener("touchstart", this.handleTouchStart, false);
+    can.addEventListener("touchmove", this.handleTouchMove, true);
+    can.addEventListener("touchend", this.handleTouchEnd, false);
+    this.offsetLeft = can.offsetLeft;
+    this.offsetTop = can.offsetTop;
+    this.ctx.font = "100px Learning Curve";
+    this.ctx.fillStyle = "red";
+    this.ctx.textAlign = "start";
+    this.ctx.fillText("abcd  efgh", 20, 90);
+    this.ctx.fillText("ijkl  mnop", 20, 190);
+    this.ctx.fillText("qrst  uvwx", 20, 290);
+    this.ctx.fillText("yz", 20, 390);
+    this.canvas = can;
+    this.initialData = this.ctx.getImageData(0, 0, can.width, can.height);
+    console.log(this.canvas.width);
   }
   drawCurve(ctx, x, y, size) {
     const { lastX, lastY } = this.state;
@@ -29,35 +45,108 @@ export default class CanvasComponent extends Component {
     ctx.fill();
     this.setState({ lastX: x, lastY: y });
   }
+  handleCompare() {
+    let rowWidth = this.canvas.width;
+    let columnHeight = this.canvas.height;
+
+    let correctPixels = 0;
+    let wrongPixels = 0;
+    let missedPixels = 0;
+    let evaluator = new Evaluator(this.initialData.data, rowWidth);
+    for (var i = 0; i < columnHeight; i++) {
+      let newImageData = this.ctx.getImageData(0, i, this.canvas.width, 1);
+      evaluator.setRowData(i, newImageData.data);
+      for (var j = 0; j < rowWidth; j++) {
+        evaluator.setCellData(j);
+        if (evaluator.isNotBlank()) {
+          if (evaluator.hasBeenDrawn()) {
+            if (evaluator.hasBeenDrawnCorrect()) {
+              evaluator.markCorrect();
+              correctPixels++;
+            } else {
+              evaluator.markWrong();
+              wrongPixels++;
+            }
+            this.ctx.putImageData(newImageData, 0, i);
+          } else {
+            missedPixels++;
+          }
+        }
+      }
+    }
+    console.log(missedPixels, correctPixels, wrongPixels);
+  }
   handleTouchStart(e) {
     this.setState({ mouseIsDown: true });
     this.handleTouchMove(e);
   }
   handleTouchMove(e) {
     e.preventDefault();
-    let canX = e.targetTouches[0].pageX - this.can.offsetLeft;
-    let canY = e.targetTouches[0].pageY - this.can.offsetTop;
+    let canX = e.targetTouches[0].pageX - this.offsetLeft;
+    let canY = e.targetTouches[0].pageY - this.offsetTop;
     if (this.state.mouseIsDown) {
-      this.drawCurve(this.ctx, canX, canY, 3);
+      this.drawCurve(this.ctx, canX, canY, 2);
     }
   }
   handleTouchEnd() {
-    this.setState({ mouseIsDown: false, lastX: 0, lastY: 0  });
+    this.setState({ mouseIsDown: false, lastX: 0, lastY: 0 });
   }
   render() {
     const { height, width } = this.props;
     return (
       <div>
+        <div>
+          <button onClick={this.handleCompare}>Compare</button>
+        </div>
         <canvas
           id="can"
           height={height}
           width={width}
-          style={{ backgroundColor: "white" }}
-          onTouchStart={this.handleTouchStart}
-          onTouchMove={this.handleTouchMove}
-          onTouchEnd={this.handleTouchEnd}
+          style={{ backgroundColor: "white", border: "1px solid blue" }}
         />
       </div>
     );
+  }
+}
+
+function Evaluator(oldArray, rowWidth) {
+  this.oldArray = oldArray;
+  this.rowWidth = rowWidth;
+
+  this.setRowData = (i, newArray) => {
+    this.i = i;
+    this.newArray = newArray;
+  };
+
+  this.setCellData = j => {
+    this.j = j;
+  };
+
+  this.isNotBlank = function() {
+    let index = 4 * this.j;
+    return !(
+      this.newArray[index] === 0 &&
+      this.newArray[index + 1] === 0 &&
+      this.newArray[index + 2] === 0 &&
+      this.newArray[index + 3] ===0
+    );
+  };
+  this.hasBeenDrawn = function() {
+    return this.newArray[4 * this.j] < 255;
+  };
+  this.hasBeenDrawnCorrect = function() {
+    let oldIndex = this.i * this.rowWidth * 4 + 4 * this.j;
+    return this.oldArray[oldIndex] > 0;
+  };
+  this.markCorrect = function() {
+    let oldIndex = this.i * this.rowWidth * 4 + 4 * this.j;
+    updateRowValues(this.newArray, 4 * this.j, [0,255,0,this.oldArray[oldIndex + 3]]);
+  };
+  this.markWrong = function(newArray, j) {
+    updateRowValues(this.newArray, 4 * this.j, [0,0,255,50]);
+  };
+
+  function updateRowValues(array, startIndex, values){
+      values.forEach((val,index) => array[startIndex+index]=val)
   }
 }
